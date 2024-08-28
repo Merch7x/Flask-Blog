3952512e527577d4a8e9143ab83698d329d6f820
Merch7x <aquamaingi@gmail.com>
2024-08-28 16:28:13 +0300

    Render followers posts

--- App/models.py
+++ App/models.py
@@ -65,6 +65,13 @@ class User(UserMixin, db.Model):
         if self.is_following(user):
             self.followed.remove(user)
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id).filter(
+                followers.c.follower_id == self.id))
+        return followed.union(self.posts).order_by(
+            Post.timestamp.desc())
+
 
 @ login.user_loader
 def load_user(id):
